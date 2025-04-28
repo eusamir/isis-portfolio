5b7468e1227f2e57f8703fec519da071f1f26eec
Samir  Andrade <samirandradee@gmail.com>
2025-04-28 10:24:24 -0300

    feat: responsivo galeria

--- src/presentation/modules/GallerySection/index.tsx
+++ src/presentation/modules/GallerySection/index.tsx
@@ -4,15 +4,18 @@ export function Gallery() {
   const images = Array(6).fill('/principal.png')
   return (
     <section className="px-4 sm:px-8 md:px-16 bg-olive py-10" id="gallery">
-      <div className="grid grid-cols-1 sm:grid-cols-2 gap-6 max-w-7xl mx-auto">
+      <div className="grid grid-cols-2 md:grid-cols-2 lg:grid-cols-2 gap-2 md:gap-4 max-w-7xl mx-auto">
         {images.map((src, index) => (
-          <div key={index} className="rounded-2xl overflow-hidden">
+          <div
+            key={index}
+            className="overflow-hidden rounded-sm md:rounded-2xl"
+          >
             <Image
               src={src}
               alt={`Imagem ${index + 1}`}
               width={600}
               height={280}
-              className="rounded-2xl object-cover"
+              className="object-cover w-full rounded-sm md:rounded-2xl"
             />
           </div>
         ))}
